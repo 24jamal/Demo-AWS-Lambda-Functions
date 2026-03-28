caf655c4542437adad503a142bba833d25de1704
Mohamed Jamaludeen <jamaludeen750@gmail.com>
2026-03-28 13:36:28 +0530

    Remove the calculate function

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,9 @@
 import json
 
 
+
 def lambda_handler(event, context):
     # TODO implement
-    result = calculate(1, 2)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
